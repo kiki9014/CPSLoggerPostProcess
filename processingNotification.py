9725812a88355aeb5e0ce991693d87a0f9ff6aae
hyunjun <kiki9014@gmail.com>
2016-08-22 23:44:14 +0900

    implement processingNotificaiton.py
    fully working

--- processingNotification.py
+++ processingNotification.py
@@ -1,12 +1,69 @@
 import base64
 import numpy as np
 import scipy.io as sio
+import os.path
 
-directoryPath = "D:/CPSLogger"
+directoryPath = "D:/SmartCampusData"
+
+messengerList = ['com.kakao.talk','jp.naver.line.android','com.Slack','com.facebook.orca']
+status = {'posted' : 0, 'removed' : 1}
+
+# data structure
+# time, type(0-SNS, 1-Noti, 2-Message), sender(-1 if type is noti), content, post/remove(0/1)
+
+class unexpectedError(Exception) :
+    def __init__(self,value) :
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+def decodeWnull(text) :
+    if text == "null" : return "null"
+    else :
+        # print(text)
+        try :
+            return base64.b64decode(text).decode("UTF-8")
+        except (UnicodeDecodeError) :
+            raise unexpectedError("decode Failed")
+
+def parsingContent(contentStr, time) :
+    timeData = [float(timeChunk) for timeChunk in time]
+    parsed = timeData
+    print(contentStr)
+    if len(contentStr) == 1 : return "null"
+    elif len(contentStr) == 3 : #SMS, MMS
+        sender = base64.b64decode(contentStr[1]).decode("UTF-8")
+        contentLength = base64.b64decode(contentStr[2]).decode("UTF-8")
+        if not contentLength.isnumeric() : contentLength = len(contentLength)
+        parsed.append(2) # 2 means SMS and MMS
+        parsed.append(int(sender))
+        parsed.append(int(contentLength))
+        parsed.append(0) # 0 means true(posted)
+    else : #other notification
+        try :
+            content = [decodeWnull(contentChunk) for contentChunk in contentStr[:-2]]
+        except unexpectedError :
+            return "error"
+        if "error" in content :
+            return "error"
+        conLen = int(contentStr[-2])
+        if content[0] in messengerList :
+            parsed.append(0)
+            parsed.append(int(content[2]))
+        else :
+            parsed.append(1)
+            parsed.append(-1)
+        stat = status[contentStr[-1][:-1]]
+        parsed.append(conLen)
+        parsed.append(stat)
+    return np.array([parsed])
 
 def extractAndSave(date, name) :
     type = "Notification"
-    with open(directoryPath+"/" + type + "/" + "CPSLogger_" + type + "_" + name + ".txt",'r') as f :
+    with open(directoryPath+"/"+name+"/CPSLogger/" + type + "/" + "CPSLogger_" + type + "_" + date + ".txt",'r') as f :
+
+        flag = False
+
         while True :
             line = f.readline()
 
@@ -14,9 +71,35 @@ def extractAndSave(date, name) :
 
             dataF = line.split(",")
 
-            print(dataF)
+            parsed = parsingContent(dataF[3:],dataF[:3])
 
-date = "2016_05_18"
+            print(parsed)
+
+            if parsed == "null" : continue
+            if parsed == "error" : break
+
+            if not flag :
+                data = np.array(parsed)
+                flag = True
+            else :
+                data = np.append(data, parsed, axis=0)
+
+        if not os.path.exists("../" + name):
+            os.mkdir("../" + name)
+        if flag :
+            sio.savemat("../" + name + "/" + type + "Data_" + date + ".mat", {type: data})
+
+
+# date = "2016_05_18"
 name = "Iron2"
 
-extractAndSave(date, name)
+extractAndSave("2016_05_18", name)
+extractAndSave("2016_05_19", name)
+extractAndSave("2016_05_20", name)
+extractAndSave("2016_05_23", name)
+extractAndSave("2016_05_24", name)
+extractAndSave("2016_05_25", name)
+extractAndSave("2016_05_26", name)
+extractAndSave("2016_05_27", name)
+extractAndSave("2016_05_31", name)
+extractAndSave("2016_06_01", name)
